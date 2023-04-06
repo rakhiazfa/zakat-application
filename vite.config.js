0d2af76fe6844853c7adea9241dd80533ab1fe3d
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-04-06 22:56:53 +0700

    Fix bug.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/template.css",
+                "resources/js/template.js",
+                "resources/js/pages/profile.js",
+                "resources/js/pages/zakat_fitrah/create.js",
+            ],
             refresh: true,
         }),
     ],
