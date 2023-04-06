198b5fb8dd76fc92d9179d280d03c2327b1f9c79
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-04-06 23:36:24 +0700

    Update zakat fitrah page.

--- resources/views/zakat_fitrah.blade.php
+++ resources/views/zakat_fitrah.blade.php
@@ -20,7 +20,7 @@
             $totalKeseluruhan = 0;
         @endphp
 
-        <div class="card">
+        <div class="card hidden md:block">
             <div class="table-responsive">
                 <table class="table table-sm">
                     <thead>
@@ -160,6 +160,146 @@
                     </tfoot>
                 </table>
             </div>
+        </div>
+
+        @php
+            $totalKeseluruhanMobile = 0;
+        @endphp
+
+        <div class="grid gap-7 md:hidden">
+
+            @foreach ($zakatFitrah as $item)
+                <div class="card p-5">
+                    <div class="flex flex-wrap justify-between items-center gap-5 mb-7">
+                        <h3 class="font-medium">{{ $item->nama_muzaki }}</h3>
+                        <span class="text-xs text-gray-500">{{ date('d F Y', strtotime($item->tanggal)) }}</span>
+                    </div>
+
+                    <div class="grid grid-cols-2 gap-5 mb-5">
+                        <div>
+                            <label class="label">Alamat</label>
+                            <span class="ml-1">{{ $item->alamat ?? '-' }}</span>
+                        </div>
+
+                        <div>
+                            <label class="label">Jumlah Jiwa</label>
+                            <span class="ml-1">{{ $item->jumlah_jiwa }}</span>
+                        </div>
+
+                        <div>
+                            <label class="label">Total Zakat</label>
+                            <span class="ml-1">{{ 'Rp. ' . number_format($item->total) }}</span>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-end items-center gap-5">
+                        <button type="button"
+                            class="w-[30px] sm:w-[35px] aspect-square grid place-items-center rounded-md bg-blue-500 hover:bg-blue-600 text-white modal-trigger"
+                            data-target="#rincianZakatFitrah-{{ $loop->iteration }}">
+                            <i class="text-[1rem] sm:text-[1.15rem] pointer-events-none uil uil-eye"></i>
+                        </button>
+
+                        <a class="w-[30px] sm:w-[35px] aspect-square grid place-items-center rounded-md bg-blue-500 hover:bg-blue-600 text-white"
+                            href="{{ route('zakat_fitrah.edit', ['zakatFitrah' => $item]) }}">
+                            <i class="text-[1rem] sm:text-[1.15rem] pointer-events-none uil uil-edit"></i>
+                        </a>
+
+                        <button type="button"
+                            class="w-[30px] sm:w-[35px] aspect-square grid place-items-center rounded-md bg-blue-500 hover:bg-blue-600 text-white modal-trigger"
+                            data-target="#hapusZakatFitrahModal-{{ $loop->iteration }}">
+                            <i class="text-[1rem] sm:text-[1.15rem] pointer-events-none uil uil-trash-alt"></i>
+                        </button>
+
+                        <div class="modal" id="rincianZakatFitrah-{{ $loop->iteration }}">
+                            <div class="modal-content-wrapper">
+                                <div class="modal-content">
+                                    <div class="header">
+                                        <h4>Rincian Zakat Fitrah</h4>
+                                    </div>
+                                    <div class="body">
+                                        <div class="mb-5">
+                                            <label class="label">Tanggal</label>
+                                            <span class="ml-1">{{ $item->tanggal }}</span>
+                                        </div>
+                                        <div class="mb-5">
+                                            <label class="label">Nama Muzaki</label>
+                                            <span class="ml-1">{{ $item->nama_muzaki }}</span>
+                                        </div>
+                                        <div class="mb-5">
+                                            <label class="label">Alamat</label>
+                                            <span class="ml-1">{{ $item->alamat ?? '-' }}</span>
+                                        </div>
+                                        <div class="mb-5">
+                                            <label class="label">Jumlah Jiwa</label>
+                                            <span class="ml-1">{{ $item->jumlah_jiwa }}</span>
+                                        </div>
+                                        <div class="mb-5">
+                                            <label class="label">Jumlah Zakat Fitrah ( Rp. )</label>
+                                            <span class="ml-1">
+                                                {{ 'Rp. ' . number_format($item->nominal_zakat_fitrah) }}
+                                            </span>
+                                        </div>
+                                        <div class="mb-5">
+                                            <label class="label">Jumlah Fidyah ( Rp. )</label>
+                                            <span class="ml-1">
+                                                {{ 'Rp. ' . number_format($item->nominal_fidyah) }}
+                                            </span>
+                                        </div>
+                                        <div class="mb-5">
+                                            <label class="label">Total ( Rp. )</label>
+                                            <span class="ml-1">{{ 'Rp. ' . number_format($item->total) }}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="footer flex justify-end gap-x-5">
+                                        <button type="button"
+                                            class="button sm bg-gray-50 hover:bg-gray-100 text-dark border modal-cancel-trigger">
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <form action="{{ route('zakat_fitrah.destroy', ['zakatFitrah' => $item]) }}" method="POST"
+                            id="hapusZakatFitrahForm-{{ $loop->iteration }}" class="hidden">
+                            @csrf
+                            @method('DELETE')
+                        </form>
+
+                        <div class="modal" id="hapusZakatFitrahModal-{{ $loop->iteration }}">
+                            <div class="modal-content-wrapper">
+                                <div class="modal-content">
+                                    <div class="header">
+                                        <h4>Apakah anda yakin?</h4>
+                                    </div>
+                                    <div class="footer flex justify-end gap-x-5">
+                                        <button type="button"
+                                            class="button sm bg-gray-50 hover:bg-gray-100 text-dark border modal-cancel-trigger">
+                                            Cancel
+                                        </button>
+                                        <button type="button"
+                                            class="button sm border border-red-500 bg-red-50 hover:bg-red-100 text-red-600 form-trigger"
+                                            data-target="#hapusZakatFitrahForm-{{ $loop->iteration }}">
+                                            Hapus
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                @php
+                    $totalKeseluruhanMobile += $item->total;
+                @endphp
+            @endforeach
+
+            <div class="card">
+                <label class="label">TOTAL KESELURUHAN</label>
+                <span class="ml-1 font-medium">{{ 'Rp. ' . number_format($totalKeseluruhanMobile) }}</span>
+            </div>
+
         </div>
 
     </section>
