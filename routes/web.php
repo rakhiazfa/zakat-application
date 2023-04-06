de30a80d3f7b0a9daf3fbd68916f00f62f65e4bb
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-04-06 22:53:09 +0700

    Auto redirect to signin route.

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+
+    return redirect()->route('auth.signin');
+});
+
 Route::name('dashboard')->middleware('auth:web', 'preventBackHistory')->group(base_path('routes/web/dashboard.php'));
 
 Route::name('profile')->middleware('auth:web')->group(base_path('routes/web/profile.php'));
